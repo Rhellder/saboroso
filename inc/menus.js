7623f97bdeb90158e008f34e3bcb43446fc23a9f
Rhellder <rodrigohcs@yahoo.com.br>
2019-01-17 08:42:59 -0200

    Projeto saboroso HCode

---
+++ inc/menus.js
@@ -0,0 +1,43 @@
+let conn = require('./db');
+
+module.exports = {
+    getMenus(){
+
+        return new Promise((resolve, reject)=>{
+            conn.query(`
+                    SELECT * FROM tb_menus ORDER BY title
+                    `, (err, results) =>{
+                        if(err)
+                        {
+                            reject(err);
+                        }
+
+                       resolve(results);
+            });
+        });
+    },
+
+    save(fields, files)
+    {
+        return new Promise((resolve, reject)=>{
+
+            conn.query(`
+            
+                INSERT INTO tb_menus (title, description, price, photo)
+                VALUES(?, ?, ?, ?)
+            
+            `,[
+                fields.title,
+                fields.description,
+                fields.price,
+                `images/${files.photo.name}`
+            ],(err, results)=>{
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    }
+};
